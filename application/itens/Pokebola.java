13fdffd890ccc90a4eb42d070cf72e67a90db5ad
enzoeferreira <enzoeferreira@gmail.com>
2024-06-22 19:19:28 -0300

    Adicionar cálculo de chances de captura

--- application/itens/Pokebola.java
+++ application/itens/Pokebola.java
@@ -2,6 +2,8 @@ package application.itens;
 
 import application.pokemon.Pokemon;
 
+import java.util.Random;
+
 public class Pokebola extends Item {
     private int chanceCaptura; // [%], 0 a 100 para pokemon com vida cheia
 
@@ -11,7 +13,16 @@ public class Pokebola extends Item {
     }
 
     public void usarItem(Pokemon alvo) {
-        // Implementar
+        Random rand = new Random();
+        int chancePokebola = rand.nextInt(100);
+
+        double hpRatio = (double) alvo.getHP() / (double) alvo.getMaxHP();
+        double chance = (1 - hpRatio)*this.chanceCaptura + this.chanceCaptura; // chanceCaptura se %HP -> 100%, 2*chanceCaptura se %HP -> 0%
+        if(chance >= chancePokebola) {
+            // Captura
+        } else {
+            // Não captura
+        }
         return;
     }
 
