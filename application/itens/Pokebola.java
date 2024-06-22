82ba96040e515cdbaf8fa97c4ab9568e23b9963c
enzoeferreira <enzoeferreira@gmail.com>
2024-06-22 16:23:16 -0300

    Adicionar certificação de que chanceCaptura não seja maior que 100%

--- application/itens/Pokebola.java
+++ application/itens/Pokebola.java
@@ -3,11 +3,11 @@ package application.itens;
 import application.pokemon.Pokemon;
 
 public class Pokebola extends Item {
-    private int chanceCaptura;
+    private int chanceCaptura; // [%], 0 a 100 para pokemon com vida cheia
 
     public Pokebola(String nome, int chanceCaptura) {
         this.nome = nome;
-        this.chanceCaptura = chanceCaptura;
+        this.chanceCaptura = Math.min(100, chanceCaptura);
     }
 
     public void usarItem(Pokemon alvo) {
