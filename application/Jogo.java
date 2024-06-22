72fea75b8f935a190ecf57c61ba8a1baf71da601
enzoeferreira <enzoeferreira@gmail.com>
2024-06-22 16:29:00 -0300

    Adicionar leitura de Pokebolas e Poções

--- application/Jogo.java
+++ application/Jogo.java
@@ -1,14 +1,23 @@
 package application;
 
 import application.pokemon.Pokemon;
+import application.itens.Pokebola;
+import application.itens.Pocao;
 
 import java.util.List;
 
 public class Jogo {
     private List<Pokemon> pokedex;
+    private List<Pokebola> pokebolas;
+    private List<Pocao> pocoes;
     private final String dataPath = "lerArquivos/../../../resource/data/";
 
     public void setPokedex(List<Pokemon> pokedex) { this.pokedex = pokedex; }
+    public void setPokebolas(List<Pokebola> pokebolas) { this.pokebolas = pokebolas; }
+    public void setPocoes(List<Pocao> pocoes) { this.pocoes = pocoes; }
+    
     public List<Pokemon> getPokedex() { return this.pokedex; }
+    public List<Pokebola> getPokebolas() { return this.pokebolas; }
+    public List<Pocao> getPocoes() { return this.pocoes; }
     public String getPath() { return this.dataPath; }
 }
---
+++ application/leituraArquivos/LerItens.java
@@ -0,0 +1,57 @@
+package application.leituraArquivos;
+
+import application.Jogo;
+import application.itens.*;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+
+public class LerItens implements I_Arquivo {
+    @Override
+    public void lerArquivo(Jogo jogo) {
+        List<Pokebola> pokebolas = new ArrayList<>();
+        List<Pocao> pocoes = new ArrayList<>();
+
+        try {
+            File filePokeball = new File(jogo.getPath() + "Pokebolas.xml");
+            File filePocoes = new File(jogo.getPath() + "Pocoes.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document docPokeball = dBuilder.parse(filePokeball);
+            Document docPocao = dBuilder.parse(filePocoes);
+            docPokeball.getDocumentElement().normalize();
+            docPocao.getDocumentElement().normalize();
+
+            // Criação de Pokebolas
+            NodeList nodeListPokeball = docPokeball.getElementsByTagName("Pokebola");
+            for(int i = 0; i < nodeListPokeball.getLength(); i++) {
+                Element pokebolaElement = (Element) nodeListPokeball.item(i);
+                
+                String nome = pokebolaElement.getElementsByTagName("nome").item(0).getTextContent();
+                int chanceCaptura = Integer.parseInt(pokebolaElement.getElementsByTagName("chanceCaptura").item(0).getTextContent());
+                Pokebola pokebola = new Pokebola(nome, chanceCaptura);
+                pokebolas.add(pokebola);
+            }
+
+            // Criação de Poções
+            NodeList nodeListPocao = docPocao.getElementsByTagName("Pocao");
+            for(int i = 0; i < nodeListPocao.getLength(); i++) {
+                Element pocaoElement = (Element) nodeListPocao.item(i);
+                
+                String nome = pocaoElement.getElementsByTagName("nome").item(0).getTextContent();
+                int cura = Integer.parseInt(pocaoElement.getElementsByTagName("cura").item(0).getTextContent());
+                Pocao pocao = new Pocao(nome, cura);
+                pocoes.add(pocao);
+            }
+        } catch(Exception e) {
+            System.err.println("Erro ao ler arquivo: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
