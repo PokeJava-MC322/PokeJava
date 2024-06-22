1f17e922a76dad834108ef6ebb3a6bd3e8fa61f6
enzoeferreira <enzoeferreira@gmail.com>
2024-06-22 19:48:38 -0300

    Adicionar método acessarItem()

--- application/itens/Inventario.java
+++ application/itens/Inventario.java
@@ -12,4 +12,17 @@ public class Inventario {
 
     public List<Item> getItens() { return this.itens; }
     public void setItens(List<Item> itens) { this.itens = itens; }
+
+    /**
+     * Procura por um item no inventário pelo seu nome
+     * 
+     * @param nome String com o nome do item a ser encontrado
+     * @return {@code Item} Se o item for encontrado, {@code null} Se o item não for encontrado
+     */
+    public Item acessarItem(String nome) {
+        for(Item item : this.itens)
+            if(nome == item.getNome())
+                return item;
+        return null;
+    }
 }
