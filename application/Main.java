e8286586d9f9669071afa52b791318818800add2
enzoeferreira <enzoeferreira@gmail.com>
2024-06-23 18:12:25 -0300

    Adicionar Menu Inicial

--- application/Main.java
+++ application/Main.java
@@ -5,10 +5,17 @@ import application.pokemon.Pokemon;
 import application.leituraArquivos.*;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+    private static final String imagePath = "application/../resource/images/";
     public static void main(String[] args) {
         launch(args);
         Jogo jogo = Jogo.getInstancia();
@@ -52,8 +59,31 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        // TODO Auto-generated method stub
+        Group root = new Group();
+        Scene scene = new Scene(root, 1280, 720);
+
+        stage.setResizable(false);
+        stage.setTitle("PokeJava");
+
+        ImageView background = new ImageView(new Image(imagePath + "/assets/menu_background.png"));
+        background.setFitWidth(scene.getWidth());
+        background.setFitHeight(scene.getHeight());
+        root.getChildren().add(background);
+
+        ImageView title = new ImageView(new Image(imagePath + "/assets/menu_title.png"));
+        title.setLayoutX(scene.getWidth()/2 - title.getBoundsInParent().getWidth()/2);
+        title.setLayoutY(0);
+        root.getChildren().add(title);
+
+        Button playButton = new Button("JOGAR");
+        playButton.setFont(Font.font("Pixelify Sans", 40));
+        playButton.setStyle("-fx-background-color: #375ca9;-fx-text-fill: #ffcc01; -fx-cursor: hand");
+        playButton.setPrefSize(350, 75);
+        playButton.setLayoutX(scene.getWidth()/2 - 350/2);
+        playButton.setLayoutY(scene.getHeight()/2 + 75/2);
+        root.getChildren().add(playButton);
+
+        stage.setScene(scene);
         stage.show();
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
     }
 }
